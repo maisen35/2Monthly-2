ac3fb09053e5ab0b11e1913c065e0af55b2796ae
EC2 Default User <ec2-user@ip-172-31-28-84.ec2.internal>
2022-02-03 09:31:57 +0000

    [Finish]2-2-6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+
+//= require jquery
+//= require rails-ujs
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,12 +10,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+require('jquery')
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+
+window.jQuery = $;
+window.$ = $;
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
